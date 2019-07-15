e0a98150c4e9720747612459171c0baacb55153e
zhangdai <284803665@qq.com>
2019-07-15 10:42:28 +0800

    数据库建模

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -3,7 +3,11 @@ from .base import *
 DEBUG=True
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'type',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
